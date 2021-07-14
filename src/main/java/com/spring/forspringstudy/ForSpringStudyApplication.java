e66ab066a28d222db7cd7641e913893bd967384b
Hyeonhee Jang <wkdgus7113@gmail.com>
2021-07-15 00:15:58 +0900

    Initial commit

---
+++ src/main/java/com/spring/forspringstudy/ForSpringStudyApplication.java
@@ -0,0 +1,13 @@
+package com.spring.forspringstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForSpringStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForSpringStudyApplication.class, args);
+	}
+
+}
